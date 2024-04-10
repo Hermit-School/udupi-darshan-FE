08919aa0905d74f32b5c5da99bd27cac3b52763e
varshini-id21 <varshinikotian21@gmail.com>
2024-04-10 21:07:35 +0400

    completed the task
    error which i mentioned previously got solved.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { DataService } from './data-service.service.';
+import { DataService } from '../../data-service.service';
 
 @Component({
   selector: 'app-home',
