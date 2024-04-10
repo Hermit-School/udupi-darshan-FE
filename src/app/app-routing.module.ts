456d98be94d8bdceaf3cd50eb417cf78cd8b5fa9
Turtle Hermit <faz2658@gmail.com>
2024-04-10 00:19:59 -0400

    bug fixed
    Co-Authored-By: varshini-id21 <149194188+varshini-id21@users.noreply.github.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 
-import { AboutComponent } from './component/about.component';
-import { ContactComponent } from './component/contact.component';
-import { HomeComponent } from './component/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './component/home.component';
-import { AboutComponent } from './component/about.component';
-import { ContactComponent } from './component/contact.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { ContactComponent } from './component/contact.component';
     AppComponent,
     HomeComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
+
   ],
   imports: [
     BrowserModule,
