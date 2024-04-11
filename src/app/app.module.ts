64d72b3aa72c51fdec7cc4e279ef12eab1851390
Sulaxmi123 <sulaxmiupadhyaya1435@gmail.com>
2024-04-11 13:22:26 +0530

    Created new component "contact-form"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ContactFormComponent } from 'src/component/contact-form/contact-form.component';
 import { FormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< Updated upstream
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
-@NgModule({
-  declarations: [
-    AppComponent
-=======
-import { ContactFormComponent } from '../component/contact-form/contact-form.component'
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    // NavbarComponent,
     ContactFormComponent
->>>>>>> Stashed changes
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-<<<<<<< Updated upstream
-    NgbModule
-=======
+    NgModule,
     FormsModule
   ],
-  providers: [
-    
->>>>>>> Stashed changes
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
