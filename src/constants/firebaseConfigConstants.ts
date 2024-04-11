c5a4551b473d78503a5474d2222af4ca2ece42ad
Turtle Hermit <faz2658@gmail.com>
2024-04-10 23:54:11 -0400

    Release v1 (#15)
    * firebase changes
    
    * firebase changes formatting
    
    * added dashboard.scss and updated firbase file
    
    * Update README.md
    
    * changes for bootstrap and google fonts
    
    * bootstrap changes
    
    * firebase file constants

---
+++ src/constants/firebaseConfigConstants.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCGTGKrUEJZsExjE9FfnqxzAsgdy1mckn8",
+    authDomain: "udupi-darshan.firebaseapp.com",
+    projectId: "udupi-darshan",
+    storageBucket: "udupi-darshan.appspot.com",
+    messagingSenderId: "184267438115",
+    appId: "1:184267438115:web:a8a321bc57fb544f5b5996",
+    measurementId: "G-H9V2CJGX4K"
+  };
