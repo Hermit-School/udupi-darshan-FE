b4358dca18483d998c36a1129afe87bd81f52e06
Turtle Hermit <faz2658@gmail.com>
2024-04-10 23:49:52 -0400

    firebase file constants

--- src/main.ts
+++ src/main.ts
@@ -7,20 +7,14 @@ import { environment } from './environments/environment';
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCGTGKrUEJZsExjE9FfnqxzAsgdy1mckn8",
-  authDomain: "udupi-darshan.firebaseapp.com",
-  projectId: "udupi-darshan",
-  storageBucket: "udupi-darshan.appspot.com",
-  messagingSenderId: "184267438115",
-  appId: "1:184267438115:web:a8a321bc57fb544f5b5996",
-  measurementId: "G-H9V2CJGX4K"
-};
+import { firebaseConfig } from "src/constants/firebaseConfigConstants";
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
