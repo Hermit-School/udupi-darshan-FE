8974aa0d805f8c06803e828e7d18900f492ec113
Sulaxmi123 <sulaxmiupadhyaya1435@gmail.com>
2024-04-22 08:50:58 +0530

    changed the json

--- src/service/config.component.ts
+++ src/service/config.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
-import { Config, ConfigService } from './config.service';
-
+import { ConfigService, Data, Glossary } from './config.service';
 @Component({
   selector: 'app-config',
   templateUrl: './config.component.html',
   styleUrls: ['./config.component.css']
 })
 export class ConfigComponent {
- 
   error: any;
-  config: Config | null = null;;
+  config: Data | null = null;
   headers: string[] = [];
 
   constructor(private configService: ConfigService) {}
@@ -17,26 +15,20 @@ export class ConfigComponent {
   clear() {
     this.config = null;
     this.error = undefined;
-    this.headers = [];
   }
 
   showConfig() {
     this.configService.getConfig()
-      .subscribe(data => {
-          console.log(data); // Check the structure and value of the data received
-          
-          // Convert the date string to a Date object
-          const date = data.date ? new Date(data.date) : null;
-  
-          this.config = {
-              heroesUrl: data.heroesUrl,
-              textfile: data.textfile,
-              date: date,
-          };
-          console.log(this.config); 
+      .subscribe((data: Data) => {
+        console.log(data); // Check the structure and value of the data received
+        
+        // Access 'title' property from 'Glossary'
+        const title: string = data.glossary.title;
+        console.log(title); // Check if title is retrieved correctly
+
+        this.config = data;
       });
   }
-  
 
   getType(val: any): string {
     return val instanceof Date ? 'date' : Array.isArray(val) ? 'array' : typeof val;
--- src/service/config.service.ts
+++ src/service/config.service.ts
@@ -3,20 +3,49 @@ import { HttpClient } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
-export interface Config {
-  heroesUrl: string;
-  textfile: string;
-  date: any;
+export interface GlossDef {
+  para: string;
+  GlossSeeAlso: string[];
 }
 
-@Injectable()
+export interface GlossEntry {
+  ID: string;
+  SortAs: string;
+  GlossTerm: string;
+  Acronym: string;
+  Abbrev: string;
+  GlossDef: GlossDef;
+  GlossSee: string;
+}
+
+export interface GlossList {
+  GlossEntry: GlossEntry;
+}
+
+export interface GlossDiv {
+  title: string;
+  GlossList: GlossList;
+}
+
+export interface Glossary {
+  title: string;
+  GlossDiv: GlossDiv;
+}
+
+export interface Data {
+  glossary: Glossary;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
 export class ConfigService {
   configUrl = 'assets/data.json';
   
   constructor(private http: HttpClient) { }
 
-  getConfig(): Observable<Config> {
-    return this.http.get<Config>(this.configUrl)
+  getConfig(): Observable<Data> {
+    return this.http.get<Data>(this.configUrl)
       .pipe(
         catchError(error => {
           console.error('Error fetching configuration:', error);
