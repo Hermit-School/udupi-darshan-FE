d778dc5c429575452e72c63a3b35b170d819d118
Sulaxmi123 <sulaxmiupadhyaya1435@gmail.com>
2024-04-29 12:29:19 +0530

    modified code

--- src/app/credit-card/credit-card.component.ts
+++ src/app/credit-card/credit-card.component.ts
@@ -8,21 +8,33 @@ import { Component, OnInit } from '@angular/core';
 export class CreditCardComponent implements OnInit {
   cardNumber: string = '';
   errorMessage: string = '';
+  isInputInvalid: boolean = false;
   ngOnInit(): void {
     
   }
 
   onCardNumberChange(event: any) {
-    // Remove non-digit characters from the input
-    this.cardNumber = event.target.value.replace(/\D/g, '');
+    // Remove all non-digit characters from the input
+    let inputDigits = event.target.value.replace(/[^0-9]/g, '');
+    if (inputDigits.length >= 19) {
+      event.preventDefault(); // Prevent further input
+      return; // Exit the function
+    }
     // Format the card number with hyphens after every 4 digits
-    this.cardNumber = this.cardNumber.replace(/(.{4})/g, '$1-');
+    this.cardNumber = inputDigits.replace(/(.{4})/g, '$1-');
+  
     // Remove the last hyphen if it's there
     this.cardNumber = this.cardNumber.replace(/-$/, '');
+  
+    // Check if the input differs from the formatted card number
+    if (event.target.value !== this.cardNumber) {
+      event.target.value = this.cardNumber; // Update the input value
+    }
+  
 
     // Check if the card number is exactly 16 digits long
     if (this.cardNumber.length !== 19) {
-      this.errorMessage = 'Card number must be 16 digits long.';
+      this.errorMessage = 'Invalid card number.';
     } else {
       this.errorMessage = '';
     }
