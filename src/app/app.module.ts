9826f96e732d92999ed480b5317e6247352c28bc
varshini-id21 <varshinikotian21@gmail.com>
2024-05-04 20:15:16 +0400

    created components
    hero-image, navigation ,advertisement and heading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeroImageComponent } from './hero-image/hero-image.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { AdvertisementComponent } from './advertisement/advertisement.component';
+import { HeadingComponent } from './heading/heading.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroImageComponent,
+    NavigationComponent,
+    AdvertisementComponent,
+    HeadingComponent
   ],
   imports: [
     BrowserModule,
