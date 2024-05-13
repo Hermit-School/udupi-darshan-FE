b93bcf134f7ad93f20baf3299004d4e57d449c2f
Turtle Hermit <faz2658@gmail.com>
2024-05-12 22:58:56 -0400

    Release v1  (#31)
    * firebase changes
    
    * firebase changes formatting
    
    * added dashboard.scss and updated firbase file
    
    * Update README.md
    
    * changes for bootstrap and google fonts
    
    * bootstrap changes
    
    * firebase file constants
    
    * added navbar and image
    
    * created a responsive page.
    
    * bootstrap icons
    
    * Modified code and project structure
    
    * Slideshow and advertisement (#25)
    
    * Update .gitignore
    
    * bootstrap changes
    
    * bento component (#26)
    
    Approved
    
    * advertisement image displaying (#28)
    
    * advertisement image displaying
    
    as you told to add one image as an advertisement and it should be responsive also.
    
    * tried horizontal scrolling
    
    could you please say the changes that i need to implement further
    
    * placed the components according to the figma design specification
    
    * latest changes
    
    * Update landing-page.component.html
    
    ---------
    
    Co-authored-by: Turtle Hermit <faz2658@gmail.com>
    
    ---------
    
    Co-authored-by: Sulaxmi123 <sulaxmiupadhyaya1435@gmail.com>
    Co-authored-by: Sulaxmi123 <131690364+sulaxmi123@users.noreply.github.com>
    Co-authored-by: varshini-id21 <149194188+varshini-id21@users.noreply.github.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+import { BentoComponent } from './components/bento/bento.component';
+import { AdvertisementComponent } from './components/advertisement/advertisement.component';
+import { CardContainerComponent } from './components/card-container/card-container.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LandingPageComponent,
+    CarouselComponent,
+    BentoComponent,
+    AdvertisementComponent,
+    CardContainerComponent,
+    
+   
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbCarouselModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
