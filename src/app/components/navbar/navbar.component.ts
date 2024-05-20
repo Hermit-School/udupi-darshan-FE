e5ed895dc2b0ff94915071da9cd27bc4bfa5e85e
Turtle Hermit <faz2658@gmail.com>
2024-05-20 11:40:21 -0400

    Release v1 (#38)
    * firebase changes
    
    * firebase changes formatting
    
    * added dashboard.scss and updated firbase file
    
    * Update README.md
    
    * changes for bootstrap and google fonts
    
    * bootstrap changes
    
    * firebase file constants
    
    * added navbar and image
    
    * created a responsive page.
    
    * bootstrap icons
    
    * Modified code and project structure
    
    * Slideshow and advertisement (#25)
    
    * Update .gitignore
    
    * bootstrap changes
    
    * bento component (#26)
    
    Approved
    
    * advertisement image displaying (#28)
    
    * advertisement image displaying
    
    as you told to add one image as an advertisement and it should be responsive also.
    
    * tried horizontal scrolling
    
    could you please say the changes that i need to implement further
    
    * placed the components according to the figma design specification
    
    * latest changes
    
    * Update landing-page.component.html
    
    ---------
    
    Co-authored-by: Turtle Hermit <faz2658@gmail.com>
    
    * added advertisements , blogs and footer. (#30)
    
    * added advertisements , blogs and footer.
    
    * unwanted code
    
    ---------
    
    Co-authored-by: Turtle Hermit <faz2658@gmail.com>
    
    * Update app.component.html
    
    * navbar update (#33)
    
    Approved
    
    * modal blog submission form. (#37)
    
    * modal blog submission form.
    
    * modified modal form.
    
    ---------
    
    Co-authored-by: Sulaxmi123 <sulaxmiupadhyaya1435@gmail.com>
    Co-authored-by: Sulaxmi123 <131690364+sulaxmi123@users.noreply.github.com>
    Co-authored-by: varshini-id21 <149194188+varshini-id21@users.noreply.github.com>

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  photoPreviews: string[] = [];
+  selectedFiles: File[] = [];
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      name: ['', [Validators.required, Validators.pattern('^[a-zA-Z ]+$')]],
+      email: ['', [Validators.required, Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$')]]
+
+    });
   }
 
+  ngOnInit(): void {
+    // JavaScript logic for character count
+    const messageInput = document.getElementById("message") as HTMLTextAreaElement;
+    const messageHelp = document.getElementById("messageHelp")!;
+
+    messageInput.addEventListener("input", function() {
+      const messageLength = messageInput.value.length;
+      messageHelp.textContent = messageLength + "/500";
+
+      // Enforce the character limit
+      if (messageLength > 500) {
+        messageInput.value = messageInput.value.substring(0, 500);
+        messageHelp.textContent = "500/500";
+      }
+    });
+  }
+
+  
+  
+  
+
+  onSubmit(): void {
+    if (this.form.valid) {
+      console.log(this.form.value);
+    } else {
+      console.log("Form is invalid");
+    }
+  }
 }
