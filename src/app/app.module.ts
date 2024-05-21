c08a80e9a034999f4e606b6b5044487472c310fa
varshini-id21 <varshinikotian21@gmail.com>
2024-05-21 06:20:18 +0400

    done with photo upload task
    have kept the photo limit to max of two photos...and only thing keft to do is set the size limit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import { CarouselComponent } from './components/carousel/carousel.component';
-import { NgbCarouselModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 import { BentoComponent } from './components/bento/bento.component';
 import { AdvertisementComponent } from './components/advertisement/advertisement.component';
 import { BlogsComponent } from './components/blogs/blogs.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { CardContainerComponent } from './components/card-container/card-container.component';
-import { ModalComponent } from './components/modal/modal.component';
-import { PopupComponent } from './components/popup/popup.component';
+import { ReactiveFormsModule } from '@angular/forms'; // Import ReactiveFormsModule and FormsModule
 
 
 
@@ -28,10 +27,8 @@ import { PopupComponent } from './components/popup/popup.component';
     AdvertisementComponent,
     BlogsComponent,
     FooterComponent,
-    CardContainerComponent,
-    ModalComponent,
-    PopupComponent,
-    
+    CardContainerComponent
+   
     
    
   ],
@@ -39,7 +36,8 @@ import { PopupComponent } from './components/popup/popup.component';
     BrowserModule,
     AppRoutingModule,
     NgbCarouselModule,
-    
+    FormsModule,
+    ReactiveFormsModule
     
   ],
   providers: [],
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,17 +1,74 @@
-// navbar.component.ts
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
-  @Output() openModalEvent = new EventEmitter<void>();
+export class NavbarComponent implements OnInit {
 
-  constructor() {}
+  form: FormGroup;
+  files: { name: string, url: string }[] = [];
+  maxFiles: number = 2;
+  
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      name: ['', [Validators.required, Validators.pattern('^[a-zA-Z ]+$')]],
+      email: ['', [Validators.required, Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$')]]
+    });
+  }
 
-  openModal() {
-    this.openModalEvent.emit();
+  ngOnInit(): void {
+    const messageInput = document.getElementById("message") as HTMLTextAreaElement;
+    const messageHelp = document.getElementById("messageHelp")!;
+
+    messageInput.addEventListener("input", function() {
+      const messageLength = messageInput.value.length;
+      messageHelp.textContent = messageLength + "/500";
+
+      if (messageLength > 500) {
+        messageInput.value = messageInput.value.substring(0, 500);
+        messageHelp.textContent = "500/500";
+      }
+    });
+  }
+
+  onSubmit(): void {
+    if (this.form.valid) {
+      console.log(this.form.value);
+    } else {
+      console.log("Form is invalid");
+    }
+  }
+
+  onFileSelected(event: any) {
+    const selectedFiles = event.target.files;
+    const uploadedPhotosContainer = document.querySelector('.uploaded-photos');
+    if (!uploadedPhotosContainer) {
+      return;
+    }
+
+    if (this.files.length + selectedFiles.length > this.maxFiles) {
+      alert(`You can only upload a maximum of ${this.maxFiles} photos.`);
+      return;
+    }
+
+    for (let file of selectedFiles) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        const fileUrl = e.target.result;
+        this.files.push({ name: file.name, url: fileUrl });
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
+  removeFile(file: { name: string, url: string }) {
+    this.files = this.files.filter(f => f !== file);
+  }
+
+  openFile(url: string): void {
+    window.open(url, '_blank');
   }
 }
