cae5c29a3edec2b65739cb916765de5245e390fd
Turtle Hermit <faz2658@gmail.com>
2024-06-10 14:24:30 -0400

    Release v1 (#57)
    * firebase changes
    
    * firebase changes formatting
    
    * added dashboard.scss and updated firbase file
    
    * Update README.md
    
    * changes for bootstrap and google fonts
    
    * bootstrap changes
    
    * firebase file constants
    
    * added navbar and image
    
    * created a responsive page.
    
    * bootstrap icons
    
    * Modified code and project structure
    
    * Slideshow and advertisement (#25)
    
    * Update .gitignore
    
    * bootstrap changes
    
    * bento component (#26)
    
    Approved
    
    * advertisement image displaying (#28)
    
    * advertisement image displaying
    
    as you told to add one image as an advertisement and it should be responsive also.
    
    * tried horizontal scrolling
    
    could you please say the changes that i need to implement further
    
    * placed the components according to the figma design specification
    
    * latest changes
    
    * Update landing-page.component.html
    
    ---------
    
    Co-authored-by: Turtle Hermit <faz2658@gmail.com>
    
    * added advertisements , blogs and footer. (#30)
    
    * added advertisements , blogs and footer.
    
    * unwanted code
    
    ---------
    
    Co-authored-by: Turtle Hermit <faz2658@gmail.com>
    
    * Update app.component.html
    
    * navbar update (#33)
    
    Approved
    
    * modal blog submission form. (#37)
    
    * modal blog submission form.
    
    * modified modal form.
    
    * created new branch (#40)
    
    named feature/nav-modal and have copied the code onto the new branch
    
    * created new branch (#42)
    
    named feature/nav-modal and have copied the code onto the new branch
    
    Co-authored-by: varshini-id21 <varshinikotian21@gmail.com>
    
    * navbar bug task (#43)
    
    * modal bug task (#45)
    
    * Feature/nav modal (#47)
    
    * created new branch
    
    named feature/nav-modal and have copied the code onto the new branch
    
    * resolved conflicts
    
    * this is the rresolved code
    
    * removed the commented lines
    
    * after removing  the css comments and the white spaces
    
    * solved the error..
    
    * Feature/nature (#51)
    
    * completed nature task
    
    * Update landing-page.component.html
    
    * culturepage componenets (#53)
    
    * reformatting culture (#56)
    
    * files not required
    
    * Update app.module.ts
    
    ---------
    
    Co-authored-by: Sulaxmi123 <sulaxmiupadhyaya1435@gmail.com>
    Co-authored-by: Sulaxmi123 <131690364+sulaxmi123@users.noreply.github.com>
    Co-authored-by: varshini-id21 <149194188+varshini-id21@users.noreply.github.com>
    Co-authored-by: varshini-id21 <varshinikotian21@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NatureComponent } from './pages/nature/nature.component';
-import { CultureComponent } from 'src/app/components/culture/culture.component';
+import { CultureComponent } from 'src/app/pages/culture/culture.component';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component'; // Assume you have a LandingPageComponent
 
 
