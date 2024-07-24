faae9f8072bc7c889cbcec0736aaf12f08654ea7
Sulaxmi123 <sulaxmiupadhyaya1435@gmail.com>
2024-07-24 08:29:22 +0530

    updated the loading file

--- src/app/services/Interceptor/MyHttpInterceptor.ts
+++ src/app/services/Interceptor/MyHttpInterceptor.ts
@@ -1,46 +1,25 @@
-// my-http-interceptor.ts
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpHandler, HttpRequest, HttpEvent } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { finalize, delay } from 'rxjs/operators';
+import { LoadingUtils } from 'src/app/util/loading-util';
 
 @Injectable()
 export class MyHttpInterceptor implements HttpInterceptor {
 
-  private activeRequests = 0;
-  private loadingElement: HTMLElement | null = null;
-  private readonly MIN_DISPLAY_TIME = 500; // Minimum display time in milliseconds
-
-  private showLoader(): void {
-    if (this.loadingElement) {
-      this.loadingElement.style.display = 'block';
-    }
-  }
-
-  private hideLoader(): void {
-    if (this.loadingElement && this.activeRequests === 0) {
-      setTimeout(() => {
-        if (this.activeRequests === 0) {
-          this.loadingElement!.style.display = 'none';
-        }
-      }, this.MIN_DISPLAY_TIME);
-    }
-  }
-
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    if (this.activeRequests === 0) {
-      this.loadingElement = document.getElementById('loading-animation');
-      this.showLoader();
+    if (LoadingUtils.activeRequests === 0) {
+      LoadingUtils.showLoader();
     }
 
-    this.activeRequests++;
+    LoadingUtils.incrementActiveRequests();
 
     return next.handle(request).pipe(
-      delay(this.MIN_DISPLAY_TIME),  // Ensure the request takes at least the minimum display time
+      delay(LoadingUtils.MIN_DISPLAY_TIME),  // Ensure the request takes at least the minimum display time
       finalize(() => {
-        this.activeRequests--;
-        if (this.activeRequests === 0) {
-          this.hideLoader();
+        LoadingUtils.decrementActiveRequests();
+        if (LoadingUtils.activeRequests === 0) {
+          LoadingUtils.hideLoader();
         }
       })
     );
---
+++ src/app/util/loading-util.ts
@@ -0,0 +1,34 @@
+
+export class LoadingUtils {
+    public static activeRequests = 0;
+    private static loadingElement: HTMLElement | null = null;
+    public static readonly MIN_DISPLAY_TIME = 500; 
+  
+    public static showLoader(): void {
+      if (!this.loadingElement) {
+        this.loadingElement = document.getElementById('loading-animation');
+      }
+      if (this.loadingElement) {
+        this.loadingElement.style.display = 'block';
+      }
+    }
+  
+    public static hideLoader(): void {
+      if (this.loadingElement && this.activeRequests === 0) {
+        setTimeout(() => {
+          if (this.activeRequests === 0) {
+            this.loadingElement!.style.display = 'none';
+          }
+        }, this.MIN_DISPLAY_TIME);
+      }
+    }
+  
+    public static incrementActiveRequests(): void {
+      this.activeRequests++;
+    }
+  
+    public static decrementActiveRequests(): void {
+      this.activeRequests--;
+    }
+  }
+  
