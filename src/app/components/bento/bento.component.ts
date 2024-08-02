084966a8ddde4edb890187143cede4fa6d71ffbb
varshini-id21 <varshinikotian21@gmail.com>
2024-08-02 07:01:01 +0400

    after implementing forloop for bento task

--- src/app/components/bento/bento.component.ts
+++ src/app/components/bento/bento.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: 'app-bento',
   templateUrl: './bento.component.html',
-  styleUrls: ['./bento.component.scss']
+  styleUrls: ['./bento.component.scss'],
 })
-export class BentoComponent implements OnInit {
 
-  constructor() { }
-
+export class BentoComponent implements OnInit{
+  cards = [
+    { id: 34, imgSrc: 'assets/images/udupi2.jpg', text: 'Krishna Matt' },
+    { id: 35, imgSrc: 'assets/images/b3.jpg', text: 'Ananteshwara temple' },
+    { id: 36, imgSrc: 'assets/images/b4.jpg', text: 'Hasta Heritage' },
+    { id: 37, imgSrc: 'assets/images/b5.jpg', text: 'Hasta Shilpa Heritage', imgClass: 'card-img-top1' },
+    { id: 38, imgSrc: 'assets/images/b6.png', text: 'Corporation Bank Heritage Museum', largeCard: true, imgClass: 'bordered-img' }
+  ];
+  constructor(){}
   ngOnInit(): void {
+    
   }
-
+  
 }
