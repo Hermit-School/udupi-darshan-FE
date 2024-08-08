ed595c8278b9ff4f15f327158780b7081ff01ae1
varshini-id21 <varshinikotian21@gmail.com>
2024-08-08 10:35:03 +0400

    made the code shorter.

--- src/app/components/bento/bento.component.ts
+++ src/app/components/bento/bento.component.ts
@@ -12,7 +12,7 @@ export class BentoComponent implements OnInit{
     { id: 35, imgSrc: 'assets/images/b3.jpg', text: 'Ananteshwara temple' },
     { id: 36, imgSrc: 'assets/images/b4.jpg', text: 'Hasta Heritage' },
     { id: 37, imgSrc: 'assets/images/b5.jpg', text: 'Hasta Shilpa Heritage', imgClass: 'card-img-top1' },
-    { id: 38, imgSrc: 'assets/images/b6.png', text: 'Corporation Bank Heritage Museum', largeCard: true, imgClass: 'bordered-img' }
+    { id: 38, imgSrc: 'assets/images/b6.png', text: 'Corporation Bank Heritage Museum', largeCard: true, imgClass: 'card-img-top bordered-img' }
   ];
   constructor(){}
   ngOnInit(): void {
