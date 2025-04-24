d1c9b150d7e8d63e42c59a9c574c5e66f84da018
Turtle Hermit <faz2658@gmail.com>
2025-04-24 10:29:29 +0530

    Update routes.ts

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -12,7 +12,7 @@ export const ApiRoutes = {
     storyData: "/v1/blogs/getBlogs"
 };
 export const Environment = {
-    local: 'http://localhost:9000',
+    local: 'http://localhost:8080',
     production: 'https://udupi-darshan-be.onrender.com',
     routes: ApiRoutes
 }
